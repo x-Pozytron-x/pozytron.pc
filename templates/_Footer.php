3cb43bd16c424136c2b210a402a6c4caefeb09b1
x-Pozytron-x <stetsenko.vitaliy@gmail.com>
2025-11-27 23:46:38 +0100

    footer links update

--- templates/_Footer.php
+++ templates/_Footer.php
@@ -1,16 +1,14 @@
 <footer class="footer card ">
   <h2 class="section-title">Let's Work Together</h2>
-  <p style="color: var(--text-secondary); font-size: 18px; max-width: 600px; margin: 0 auto;">
+  <p>
     I'm always interested in new opportunities and exciting projects. 
     Feel free to reach out if you'd like to collaborate!
   </p>
   <div class="contact-info">
-    <div class="contact-item">
-      <strong>Email:</strong> stetsenko.vitaliy@gmail.com
-    </div>
-    <div class="contact-item">
-      <strong>Phone:</strong> +380 96 085-93-96
-    </div>
+
+    <a href="mailto:stetsenko.vitaliy@gmail.com" class="contact-item" target="_blank" aria-label="Github">
+      <i class="fa fa-envelope"></i>
+    </a>
 
     <a href="https://github.com/x-Pozytron-x?tab=overview" class="contact-item" target="_blank" aria-label="Github">
       <i class="fa fa-github"></i>
