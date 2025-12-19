ce76d85faec4a4bd580cb8806d154809ce6539b5
x-Pozytron-x <stetsenko.vitaliy@gmail.com>
2025-12-19 23:36:26 +0100

    multilang to experience

--- templates/_Experience.php
+++ templates/_Experience.php
@@ -1,5 +1,8 @@
 <?php
   $experience = get_data("SELECT * FROM pozytron_experience");
+
+  $exp_title = "experience_title_" . $lang;
+  $exp_descr = "experience_descr_" . $lang;
 ?>
 
 <div class="card fade-in">
@@ -7,20 +10,20 @@
 
   <div class="timeline">
 
-  <? foreach($experience as $key => $value)  {
+    <? foreach($experience as $key => $value)  {
     echo '<div class="timeline-item">';
       echo '<div class="timeline-dot"></div>';
       echo '<div class="timeline-content">';
         echo '<div class="job-period">'. $value["experience_start"] . ' - ' . $value["experience_end"] .'</div>'; 
-        echo '<h4 class="job-title">'. $value["experience_title_en"] .'</h4>';
+        echo '<h4 class="job-title">'. $value[$exp_title] .'</h4>';
         echo '<div class="company-name">'. $value["experience_company"] .'</div>';
         echo '<div class="job-location">'. calculate_date_difference($value["experience_start"], $value["experience_end"]) .'</div>';
         echo '<p class="job-description">';
-          echo $value["experience_descr_en"];
+          echo $value[$exp_descr];
         echo '</p>';
       echo '</div>';
     echo '</div>';
   } ?>
 
-  </div> 
+  </div>
 </div>
